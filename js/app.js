756d11afd35127ad46ebfb5e324e6d7fc296420e
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-10-01 20:04:52 -0300

    capturando js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 console.log("funcionando");
+
+const formulario = document.getElementById("formulario");
+const pintarToDo = document.getElementById("pintarToDo");
+const templateTareas = document.getElementById("alertasTareas").content;
