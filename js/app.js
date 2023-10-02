74d0ccad2ae255162b0414b32a5ae976a13fbc77
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-10-01 22:32:02 -0300

    modificaciones html y app.js

--- js/app.js
+++ js/app.js
@@ -5,24 +5,77 @@ const pintarToDo = document.getElementById("pintarToDo");
 const templateTareas = document.getElementById("alertasTareas").content;
 const alert = document.querySelector(".alert");
 
-let tareas= [];
+let tareas = [];
 
-formulario.addEventListener("submit",e=>{
+document.addEventListener("click", (event) =>{
+  
+    console.log(event.target);
+
+
+
+
+});
+
+
+
+
+
+formulario.addEventListener("submit", e => {
     e.preventDefault();
     // console.log("fomulario funcionando");
 
     const data = new FormData(formulario);
     const [todo] = [...data.values()]; //esto devuelve un array de los elementos y los valores de estos que contiene los input en este caso solo capturamos un input con su valor
 
-    console.log(todo);
 
     alert.classList.add("d-none");
 
-    if (!todo.trim()){
+    if (!todo.trim()) {
         console.log("Escriba una tarea para poder agregarla a la lista");
         alert.classList.remove("d-none");
         return;
     }
 
+    agregarTodo(todo);
+    pintarTarea();
 
 });
+
+
+
+const agregarTodo = (todo) => {
+
+    const tarea = {
+        nombre: todo,
+        id: `${Date.now()}`
+    }
+
+    tareas.push(tarea);
+    // console.log(tareas);
+
+}
+
+const pintarTarea = () => {
+
+    pintarToDo.textContent = "";
+    const fragment = document.createDocumentFragment();
+
+    tareas.forEach(item => {
+        const clone = templateTareas.cloneNode(true);
+        clone.querySelector("p").textContent = item.nombre;
+        clone.querySelector(".eliminar").dataset.id = item.id;
+        fragment.appendChild(clone);
+        
+    });
+
+    pintarToDo.appendChild(fragment);
+
+}
+
+
+
+
+
+
+
+
